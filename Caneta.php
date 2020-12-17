b3086213b939338a9a6655412e3ea439546424c8
Evelyn Schmitz <evelynschmitz1@gmail.com>
2020-12-16 21:50:07 -0300

    feat(aula 3): configurando a visibilidade de atributos e metodos

--- Caneta.php
+++ Caneta.php
@@ -1,12 +1,12 @@
 <?php
 class Caneta {
-  var $modelo;
-  var $cor;
-  var $ponta;
-  var $carga;
-  var $tampada;
+  public $modelo;
+  public $cor;
+  private $ponta;
+  protected $carga;
+  protected $tampada;
 
-  function rabiscar() {
+  public function rabiscar() {
     if ($this->tampada) {
       echo "Erro. A caneta está tampada!";
     } else {
@@ -14,11 +14,11 @@ class Caneta {
     }
   }
 
-  function tampar() {
+  public function tampar() {
     $this->tampada = true;
   }
 
-  function destampar() {
+  public function destampar() {
     $this->tampada = false;
   }
 }
--- index.php
+++ index.php
@@ -2,16 +2,14 @@
   require_once 'Caneta.php';
 
   $c1 = new Caneta();
+  $c1->modelo = "BIC cristal";
   $c1->cor = "Azul";
-  $c1->ponta = 0.5;
-  $c1->tampada = false;
-  $c1->tampar();
+  // $c1->ponta = 0.5;
+  // $c1->carga = 99;
+  // $c1->tampada = true;
   $c1->rabiscar();
-  var_dump($c1);
-
-  $c2 = new Caneta();
-  $c2->cor = "Verde";
-  $c2->carga = 50;
-  $c2->tampar();
-  var_dump($c2);
+  $c1->tampar();
+  $c1->destampar();
+  
+  print_r($c1);
 ?>
